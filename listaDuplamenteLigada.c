ec68ee83bf7e8249f23a9bf913a78062c9013fc8
Vinícius Yudi Oya <viniciusyudi10043@gmail.com>
2025-01-06 02:44:11 -0300

    Update listaDuplamenteLigada.c

--- listaDuplamenteLigada.c
+++ listaDuplamenteLigada.c
@@ -1,9 +1,3 @@
-// Alterações importantes:
-// -> adicionado um ponteiro que aponta para o próximo elemento, e para o anterior (lista duplamente ligada)
-// -> com o ponteiro anterior agora, algumas verificações extras são necessárias em ambas as funções: inserir e excluir.
-// -> se o anterior eh NULO na função de inserção, é em razão de ser o primeiro elemento da lista, lógica similar ou idêntica à de exclusão
-// * com isso, a economia de memória eh maior e é mais fácil iterar sobre a lista
-
 #include "listaDuplamenteLigada.h"
 #include <stdio.h>
 #include <stdlib.h>
